79125008e80b19f43ec15c57f4ce080b7d9921f3
Stefan Rau <stefan.rau@makeittrue.de>
2022-11-09 14:01:18 +0100

    Update

---
+++ lib/FrequencyCounter/Application.cpp
@@ -0,0 +1,531 @@
+// Arduino Frequency Counter
+// 31.10.2022
+// Stefan Rau
+// History
+// 31.10.2022: 1st version - Stefan Rau
+
+#include "Application.h"
+
+static Application *gInstance = nullptr;
+
+// Module
+static Counter *gCounter = nullptr;
+static LCDHandler *gLCDHandler = nullptr;
+static FrontPlate *gFrontPlate = nullptr;
+static ModuleFactory *gModuleFactory = nullptr;
+
+static bool gIsInitialized = false;
+static bool gReadEventCounter;
+
+// Text definitions
+
+/// <summary>
+/// There is no new EEPROM address required
+/// </summary>
+TextMain::TextMain(int iSettingsAddress) : TextBase(iSettingsAddress)
+{
+}
+
+TextMain::~TextMain()
+{
+}
+
+String TextMain::GetObjectName()
+{
+    return "Main";
+}
+
+Application *Application::GetInstance()
+{
+    gInstance = (gInstance == nullptr) ? new Application() : gInstance;
+    return gInstance;
+}
+
+#ifndef DEBUG_APPLICATION
+String TextMain::FreeMemory(int iFreeMemory)
+{
+    switch (GetLanguage())
+    {
+        TextLangE("Free memory: " + String(iFreeMemory) + " byte");
+        TextLangD("Freier Speicher: " + String(iFreeMemory) + " Byte");
+    }
+}
+#endif
+
+String TextMain::ErrorInSetup()
+{
+    switch (GetLanguage())
+    {
+        TextLangE("Error in setup");
+        TextLangD("Fehler im setup");
+    }
+}
+
+String TextMain::ErrorInLoop()
+{
+    switch (GetLanguage())
+    {
+        TextLangE("Error in loop");
+        TextLangD("Fehler in loop");
+    }
+}
+
+/////////////////////////////////////////////////////////////
+
+Application::Application()
+{
+}
+
+Application::~Application()
+{
+}
+
+void Application::setup()
+{
+    // Set clock frequency of I2C to 100kHz
+    Wire.begin();
+    delay(10);
+
+#ifdef EXTERNAL_EEPROM
+    ProjectBase::SetI2CAddressGlobalEEPROM(mInitializeSystem.EEPROM.I2CAddress);
+#endif
+    mText = new TextMain(mInitializeSystem.Text.SettingsAddress);
+
+    DebugPrint("Start Setup");
+
+    //// CPU board
+
+    // Input 0.5 Hz
+    pinMode(cI0_5Hz, INPUT);
+    // Output Reset 0.5 Hz counter
+    pinMode(cOReset0_5Hz, OUTPUT);
+    // Input period measurement done
+    pinMode(cIDone, INPUT);
+    // Output reset period measurement
+    pinMode(cONotResetPeriod, OUTPUT);
+    // Output reset counter
+    pinMode(cOResetCounter, OUTPUT);
+    // Output reset input driver of 0.5 Hz counter
+    pinMode(cOResetFF, OUTPUT);
+
+    // LCD
+    if (!ErrorDetected())
+    {
+        gLCDHandler = LCDHandler::GetInstance(mInitializeSystem.LCDHandler);
+    }
+
+    // Reset input modules and start lamp test
+    if (!ErrorDetected())
+    {
+        gModuleFactory = ModuleFactory::GetInstance(mInitializeSystem.ModuleFactory);
+        gModuleFactory->I2ELampTestOn();
+    }
+
+    // Initialize main counter
+    if (!ErrorDetected())
+    {
+        gCounter = Counter::GetInstance(mInitializeSystem.Counter);
+        gCounter->I2ESetFunctionCode(Counter::eFunctionCode::TFrequency);
+    }
+
+    // Initialize front plate
+    if (!ErrorDetected())
+    {
+        // Reset selection and start
+        gFrontPlate = FrontPlate::GetInstance(mInitializeSystem.FrontPlate, gLCDHandler, gModuleFactory, gCounter);
+    }
+
+    // Initialize task management and all tasks
+    DebugPrint("Initialize tasks");
+
+    // Task for lamp test end
+    mLampTestTime = Task::GetNewTask(Task::TOneTime, 20, TaskLampTestEnd);
+
+    // Task timer for switching off the menue in LCD
+    mMenuSwitchOfTime = Task::GetNewTask(Task::TTriggerOneTime, 20, TaskMenuSwitchOff);
+    mMenuSwitchOfTime->DefinePrevious(mLampTestTime);
+
+    // Task timer LCD refresh
+    mLCDRefreshCycleTime = Task::GetNewTask(Task::TFollowUpCyclic, 10, TaskLCDRefresh);
+    mLCDRefreshCycleTime->DefinePrevious(mLampTestTime);
+
+    // Initialize task handler
+    TaskHandler::GetInstance()->SetCycleTimeInMs(100);
+
+    // Reset
+    ResetCounters();
+    RestartGateTimer();
+    RestartPulsDetection();
+
+#ifndef DEBUG_APPLICATION
+    // Initialize remote control
+    if (!ErrorDetected())
+    {
+        mRemoteControl = RemoteControl::GetInstance(mRemoteControlBuffer, 80);
+    }
+#endif
+
+    // Output potential errors
+    if (ErrorDetected())
+    {
+        if (gLCDHandler != nullptr)
+        {
+            gLCDHandler->SetErrorText(mText->ErrorInSetup());
+        }
+        DebugPrint("Error in setup");
+    }
+
+    DebugPrint("End setup");
+}
+
+void Application::loop()
+{
+    String lCommand = "";
+    long lFreeMemory;
+
+    lFreeMemory = GetFreeRAM();
+    if (mFreeMemory != lFreeMemory)
+    {
+        mFreeMemory = lFreeMemory;
+        DebugPrint("Free Memory: " + String(mFreeMemory));
+    }
+
+    if (gLCDHandler != nullptr)
+    {
+        gLCDHandler->loop(); // LCD must be called before the hardware is initialized to show potential errors
+    }
+
+    if (!gIsInitialized)
+    {
+        return;
+    }
+
+#ifndef DEBUG_APPLICATION
+    // dispatch the different modules
+    mRemoteControl->Read();
+    lCommand = String(mRemoteControlBuffer);
+    if (lCommand != "")
+    {
+        int lSeparatorIndex;
+        char lModule;
+        char lParameter;
+        String lReturn = "";
+
+        lSeparatorIndex = lCommand.indexOf(':');
+
+        if (lSeparatorIndex == 1)
+        {
+            lModule = lCommand[0];
+            lParameter = lCommand[2];
+
+            switch (lModule)
+            {
+            case 'S':
+
+                switch (lParameter)
+                {
+
+                case 'N':
+                    // Reads the name of the device
+                    lReturn = String(DEVICENAME);
+                    break;
+
+                case 'V':
+                    // Reads version and lists all hardware components and their state
+                    lReturn = String(VERSION) + ", compiled at: " + String(__DATE__) + "\n";
+                    lReturn += ErrorHandler::GetInstance()->GetStatus();
+                    lReturn += gLCDHandler->GetStatus();
+                    lReturn += gFrontPlate->GetStatus();
+                    lReturn += gCounter->GetStatus();
+                    lReturn += gModuleFactory->GetStatus();
+                    lReturn += mText->FreeMemory(GetFreeRAM());
+                    break;
+
+                case 'v':
+                    // verbose mode
+                    ProjectBase::SetVerboseMode(true);
+                    lReturn = String(lParameter);
+                    break;
+
+                case 's':
+                    // short mode
+                    ProjectBase::SetVerboseMode(false);
+                    lReturn = String(lParameter);
+                    break;
+                }
+                break;
+
+            case 'D':
+
+                // Reads the current measurement value
+                lReturn = mMeasurementValue;
+                break;
+            }
+
+#ifdef EXTERNAL_EEPROM
+            if (lReturn == "")
+            {
+                // Manage error handling
+                // lModule = 'E'	: Code for this class, if controlled remotely
+                // lParameter = 'F' : Formatting EEPROM
+                // lParameter = '0' : Reset Log Pointer
+                // lParameter = 'R' : Read log
+                // lParameter = 'S' : Read log size
+                lReturn = ErrorHandler::GetInstance()->Dispatch(lModule, lParameter);
+            }
+#endif
+
+            if (lReturn == "")
+            {
+                // Select or get the current input module
+                // lModule = 'M'	: Code for this class, if controlled remotely
+                // lParameter = 'T' : 100 MHz TTL / CMOS module
+                // lParameter = 'A' : 100 MHz analog module
+                // lParameter = 'H' : 10GHz module
+                // lParameter = 'N' : no module - dummymodule for test purposes and fallback if no module is installed
+                // lParameter = '*' : codes of all installed modules "TAHN" - returns comma separated, readable text in verbose mode
+                // lParameter = '?' : returns the code of the selected module: 'T', 'A', 'H' or 'N' - returns readable text in verbose mode
+                lReturn = gModuleFactory->Dispatch(lModule, lParameter);
+            }
+
+            if (lReturn == "")
+            {
+                // Select / display function
+                // lModule = 'F'	: Code for this class, if controlled remotely
+                // lParameter = 'f' : Frequenz
+                // lParameter = 'P' : Duration of positive level
+                // lParameter = 'N' : Duration of negative level
+                // lParameter = 'p' : Period triggered by positive edge
+                // lParameter = 'n' : Period triggered by negative edge
+                // lParameter = 'C' : Event counting
+                // lParameter = '*' : codes of all functions supported by the module "fnpNP" - returns comma separated, readable text in verbose mode
+                // lParameter = '?' : returns the code of the selected function: 'f', 'n', 'p', 'N', 'P', 'C' or '-' (if no function is selected)
+
+                // Select / display menue entry
+                // lModule = 'K'
+                // lParameter = '0' .. '9'	: Number of the menu item to select
+                // lParameter = '*' 		: codes of all menu items "123..." - returns comma separated, readable text in verbose mode
+                // lParameter = '?'			: returns the number of the currently selected menu item - returK:0ns readable text in verbose mode
+                lReturn = gFrontPlate->Dispatch(lModule, lParameter);
+            }
+
+            if (lReturn == "")
+            {
+                // Select or get the current language
+                // lModule = 'L'			: Code for this class, if controlled remotely
+                // lParameter = 'D', 'E'	: Select language
+                // lParameter = '*'			: Lists all installed language as string of language codes - todo: im verbose mode komagetrennte Texte
+                // lParameter = '?'			: Shows the current language code: 'D', 'E' - todo: im verbose mode den Klartext
+                lReturn = mText->Dispatch(lModule, lParameter);
+                if ((lReturn != "") && (lParameter != '*') && (lParameter != '?'))
+                {
+                    gFrontPlate->TriggerLampTestOff(); // Reload displayed texts
+                }
+            }
+
+            if (lReturn != "")
+            {
+                Serial.println(lReturn + "#");
+            }
+        }
+    }
+#endif
+
+    // In case of an error, the program stops here
+    if (ErrorHandler::GetInstance()->ContainsErrors())
+    {
+        if (!mErrorPrinted)
+        {
+            gLCDHandler->SetErrorText(mText->ErrorInLoop());
+            DebugPrint("Error in runtime => processing stopped");
+            mErrorPrinted = true;
+        }
+        return;
+    }
+
+    DebugLoop();
+    gFrontPlate->loop();
+    gModuleFactory->loop();
+    gCounter->loop();
+
+    // Reset I2C after an error was detected
+    if (Wire.getWriteError() != 0)
+    {
+        Wire.end();
+        Wire.begin();
+        DebugPrint("Reset I2C");
+    }
+
+    // Get current menu item if a new one was selected
+    if (gFrontPlate->IsNewMenuSelected())
+    {
+        DebugPrint("Trigger TaskMenuSwitchOff");
+        mMenuSwitchOfTime->Restart();
+        ModuleBase *lModuleBase = gModuleFactory->GetSelectedModule();
+        gLCDHandler->TriggerMenuSelectedFunction(lModuleBase->GetCurrentMenuEntry(-1), lModuleBase->GetCurrentMenuEntryNumber(), lModuleBase->GetLastMenuEntryNumber());
+        DebugPrint("Selected menu entry: " + lModuleBase->GetCurrentMenuEntry(-1));
+    }
+
+    // Reset counter if a new function is selected
+    if (gFrontPlate->IsNewFunctionSelected())
+    {
+        ResetCounters();
+        mMeasurementValue = gCounter->I2EGetCounterValue();
+        RestartPulsDetection();
+        RestartGateTimer();
+        mEventCountingInitialized = false;
+        gReadEventCounter = false;
+    }
+
+    if (gCounter->GetFunctionCode() == Counter::eFunctionCode::TFrequency)
+    {
+        // When frequency is selected
+        if (digitalRead(cI0_5Hz) == LOW) // check if 10.000.000 pulses were counted
+        {
+            // DebugPrint("0.5 Hz signal detected");
+            //  Wait a bit until the value is read
+            //  => the counters are connected in a chain, it may happen some micro seconds until the last pulse reaches the last counter
+            delayMicroseconds(10);
+            mMeasurementValue = gCounter->I2EGetCounterValue();
+
+            digitalWrite(cOResetCounter, HIGH);
+            delayMicroseconds(10);
+
+            digitalWrite(cOResetFF, HIGH);
+            delayMicroseconds(10);
+
+            digitalWrite(cOReset0_5Hz, HIGH);
+            delayMicroseconds(10);
+
+            digitalWrite(cOReset0_5Hz, LOW);
+            delayMicroseconds(10);
+
+            digitalWrite(cOResetCounter, LOW);
+            delayMicroseconds(10);
+
+            digitalWrite(cOResetFF, LOW);
+            delayMicroseconds(10);
+        }
+    }
+    else if (gCounter->GetFunctionCode() == Counter::eFunctionCode::TEventCounting)
+    {
+        // Event counting used frequency counter input, but does not use 0.5Hz => that is set permanently to 1
+        // DebugPrint("Count events");
+        if (!mEventCountingInitialized)
+        {
+            digitalWrite(cOReset0_5Hz, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(cOResetFF, LOW);
+            delayMicroseconds(10);
+            mEventCountingInitialized = true;
+        }
+        if (gReadEventCounter)
+        {
+            mMeasurementValue = gCounter->I2EGetCounterValue();
+            gReadEventCounter = false;
+        }
+    }
+    else
+    {
+        // Pulse length
+        if (digitalRead(cIDone) == HIGH) // check if pulse end is detected
+        {
+            // DebugPrint("Trigger detected");
+            //   Wait a bit until the value is read
+            delayMicroseconds(100);
+            mMeasurementValue = gCounter->I2EGetCounterValue();
+            ResetCounters();
+            RestartPulsDetection();
+        }
+    }
+
+    gLCDHandler->SetMeasurementValue(mMeasurementValue);
+}
+
+void Application::TaskLampTestEnd()
+{
+    DebugPrintFromTask("TaskLampTestEnd\n");
+
+    // Switch off initialization message of LCD
+    if (gLCDHandler != nullptr)
+    {
+        gLCDHandler->TriggerShowCounter();
+    }
+
+    // End lamp test at front plate and get stored function
+    if (gFrontPlate != nullptr)
+    {
+        gFrontPlate->TriggerLampTestOff();
+        DebugPrintFromTask("Initial function: " + gCounter->GetSelectedFunctionName() + "\n");
+    }
+
+    // End lamp test at all modules
+    if (gModuleFactory != nullptr)
+    {
+        gModuleFactory->TriggerLampTestOff();
+        DebugPrintFromTask("Initial module: " + gModuleFactory->GetSelectedModule()->GetName() + "\n");
+        DebugPrintFromTask("Initial menu entry: " + gModuleFactory->GetSelectedModule()->GetCurrentMenuEntry(-1) + "\n");
+    }
+
+    gIsInitialized = true;
+}
+
+void Application::TaskMenuSwitchOff()
+{
+    // Switch off menu message on display
+    // DebugPrintFromTask("TaskMenuSwitchOff\n");
+    if (gLCDHandler != nullptr)
+    {
+        gLCDHandler->TriggerShowCounter();
+    }
+}
+
+void Application::TaskLCDRefresh()
+{
+    // DebugPrintFromTask("LCDRefresh");
+    gReadEventCounter = true;
+    if (gLCDHandler != nullptr)
+    {
+        gLCDHandler->TriggerShowRefresh();
+    }
+}
+
+long Application::GetFreeRAM()
+{
+    char top;
+#ifdef __arm__
+    return &top - reinterpret_cast<char *>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+    return &top - __brkval;
+#else  // __arm__
+    return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif // __arm__
+    return 0;
+}
+
+void Application::ResetCounters()
+{
+    // reset counters
+    digitalWrite(cOResetCounter, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(cOResetCounter, LOW);
+    delayMicroseconds(10);
+}
+
+void Application::RestartPulsDetection()
+{
+    // Restart puls detection
+    digitalWrite(cONotResetPeriod, LOW);
+    delayMicroseconds(10);
+    digitalWrite(cONotResetPeriod, HIGH);
+    delayMicroseconds(10);
+}
+
+void Application::RestartGateTimer()
+{
+    // restart 0.5Hz
+    digitalWrite(cOReset0_5Hz, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(cOReset0_5Hz, LOW);
+    delayMicroseconds(10);
+}
---
+++ lib/FrequencyCounter/Application.h
@@ -0,0 +1,157 @@
+// Arduino Frequency Counter
+// 31.10.2022
+// Stefan Rau
+// Main program
+
+#pragma once
+#ifndef _Application_h
+#define _Application_h
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#include "Debug.h"
+#include "Taskhandler.h"
+#include "ModuleFactory.h"
+#include "FrontPlate.h"
+#include "Counter.h"
+#include "LCDHandler.h"
+#include "RemoteControl.h"
+#include "ErrorHandler.h"
+
+#define VERSION "V 1"
+#define DEVICENAME "Frequenzzaehler 1"
+
+/// <summary>
+/// Local text class of the module
+/// </summary>
+class TextMain : public TextBase
+{
+public:
+    TextMain(int iSettingsAddress);
+    ~TextMain();
+
+    String GetObjectName() override;
+#ifndef DEBUG_APPLICATION
+    String FreeMemory(int iFreeMemory);
+#endif
+    String ErrorInSetup();
+    String ErrorInLoop();
+};
+
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char *sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif // __arm__
+
+class Application
+{
+public:
+    // Hardware
+
+    // Period detected
+    const uint8_t cIDone = 6;
+    // Input 0.5Hz counter
+    const uint8_t cI0_5Hz = 7;
+    // Reset counter input driver
+    const uint8_t cOResetFF = 10;
+    // Reset counter
+    const uint8_t cOResetCounter = 11;
+    // Reset period detection
+    const uint8_t cONotResetPeriod = 12;
+    // Reset 0.5Hz counter
+    const uint8_t cOReset0_5Hz = 13;
+
+    struct sInitializeSystem
+    {
+        I2CBase::sInitializeModule EEPROM = {-1, 0, 0x50};          // EEPROM is not used, I2C uses 0x50
+        I2CBase::sInitializeModule Text = {0x00, 1, -1};            // EEPROM uses 0x00, I2C is not used
+        I2CBase::sInitializeModule Counter = {-1, 0, 0x20};         // EEPROM is not used, I2C uses 2 addresses 0x20 .. 0x21
+        I2CBase::sInitializeModule ModuleFactory = {0x02, 8, 0x22}; // EEPROM uses 4 addresses 0x02 .. 0x05, I2C uses 3 addresses 0x22 .. 0x25
+        I2CBase::sInitializeModule LCDHandler = {0x06, 0, 0x26};    // EEPROM and I2C is used
+        I2CBase::sInitializeModule FrontPlate = {0x07, 1, 0x27};    // EEPROM and I2C is used
+        I2CBase::sInitializeModule ErrorLogger = {-1, 0, -1};       // EEPROM is not used
+    } mInitializeSystem;
+
+private:
+    TextMain *mText = nullptr;      // Pointer to current text objekt of main
+    bool mErrorPrinted;             // Signals than an error in the main loop is outputted
+    bool mEventCountingInitialized; // Event counting shall be initialized only once after selected
+    long mFreeMemory;
+
+#ifndef DEBUG_APPLICATION
+    RemoteControl *mRemoteControl = nullptr;
+#define RemoteControlBufferSize 80
+    char mRemoteControlBuffer[RemoteControlBufferSize];
+#endif
+
+    // Tasks
+    Task *mLampTestTime = nullptr;
+    Task *mMenuSwitchOfTime = nullptr;
+    Task *mLCDRefreshCycleTime = nullptr;
+
+    String mMeasurementValue = "";
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    Application();
+    ~Application();
+
+public:
+    /// <summary>
+    /// Gets a singleton.
+    /// </summary>
+    /// <returns>Instance of this class</returns>
+    static Application *GetInstance();
+
+    /// <summary>
+    /// Called by framework one time
+    /// </summary>
+    void setup();
+
+    /// <summary>
+    /// Called by framework periodically
+    /// </summary>
+    void loop();
+
+    /// <summary>
+    /// Task that stops initialization phase: switches off LEDs, switches off initialization message on LCD
+    /// </summary>
+    static void TaskLampTestEnd();
+
+    /// <summary>
+    /// Task that switches off menu message on LCD after a menu button was pressed
+    /// </summary>
+    static void TaskMenuSwitchOff();
+
+    /// <summary>
+    /// Task that refreshs the LCD periodically
+    /// </summary>
+    static void TaskLCDRefresh();
+
+    /// <summary>
+    /// Get the really free RAM of the processor
+    /// </summary>
+    /// <returns>Free RAM in byte</returns>
+    long GetFreeRAM();
+
+    /// <summary>
+    /// Resets the counters
+    /// </summary>
+    void ResetCounters();
+
+    /// <summary>
+    /// Resets pulse recognition
+    /// </summary>
+    void RestartPulsDetection();
+
+    /// <summary>
+    /// Restart 0.5Hz
+    /// </summary>
+    void RestartGateTimer();
+};
+
+#endif
